d5c1ea08f15a2f7de54cb5dd3724420e5daa9981
Mariam <mariam1990@ok.de>
2017-01-31 21:06:22 +0400

    edit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
-  #def index
-    #@products = Product.all    
-    #@featured_product = Product.first
-  #end
-  
   def index
-     @products = Product.limit(3)
+    @products = Product.all    
+    @featured_product = Product.first
   end
+  
+  #def index
+     #@products = Product.limit(3)
+  #end
 
 end
