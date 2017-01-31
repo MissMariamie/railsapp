598ff3184d8a2119f443e1e4bb3410e4479a80a0
Mariam <mariam1990@ok.de>
2017-01-31 21:24:50 +0400

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   
   root 'static_pages#about'
   
-  
-  
   get '/products/:id', to: 'products#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
